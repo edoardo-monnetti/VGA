36a56c3c2d442e3b62d7a956708a62a766dd958d
Edoardo Monnetti <edoardo.monnetti@edu.unito.it>
2021-08-28 18:24:25 +0200

    added testbench

--- work/sim/xsim.dir/display_tb/xsim_script.tcl
+++
@@ -1,4 +0,0 @@
-set_param project.enableReportConfiguration 0
-load_feature core
-current_fileset
-xsim {display_tb} -wdb {display_tb.wdb} -autoloadwcfg -tclbatch {/home/edoardo/Desktop/VGA_project/work/sim/../../scripts/sim/run.tcl} -onerror stop -stats
